025837255bd72d1805242f1f601b0cf1c1d8e73b
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-27 01:04:24 +0530

    ✨ setup google sign-in

--- src/App.js
+++ src/App.js
@@ -7,18 +7,41 @@ import HomePage from "./pages/homepage/homepage.componenet";
 import ShopPage from "./pages/shop/shop.componenet";
 import Header from "./components/header/header.componenet";
 import SignInUp from "./pages/sign-in-up/sign-in-up.componenet";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unSubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
