68bb520760887fb0a438c0951d769b005debb24b
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-26 17:29:24 +0530

    ✨ add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.componenet';
+import ShopPage from './pages/shop/shop.componenet';
 
 function App() {
   return ( 
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
