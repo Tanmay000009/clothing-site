fe8784e622d282651199002924d0679a21a57e2a
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-27 00:16:19 +0530

    import sign in sign up component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.componenet';
-import ShopPage from './pages/shop/shop.componenet';
-import Header from './components/header/header.componenet';
+import HomePage from "./pages/homepage/homepage.componenet";
+import ShopPage from "./pages/shop/shop.componenet";
+import Header from "./components/header/header.componenet";
+import SignInUp from "./pages/sign-in-up/sign-in-up.componenet";
 
 function App() {
-  return ( 
+  return (
     <div>
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInUp} />
       </Switch>
     </div>
   );
