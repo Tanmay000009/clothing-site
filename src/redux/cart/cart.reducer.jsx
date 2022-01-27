98b6fdcab78182cfd240a11cafafa68f1bac5922
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-27 17:31:03 +0530

    ✏️ fix cart state not changing

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.tye) {
+  switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
