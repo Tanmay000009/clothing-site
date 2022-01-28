da00e8a560bbc692f4b52205d686e72147914460
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-28 17:42:14 +0530

    ✨ add routes

--- src/pages/shop/shop.componenet.jsx
+++ src/pages/shop/shop.componenet.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
-
-const ShopPage = ({ collections }) => (
+import CollectionPage from "../collection/collection.component";
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverview />
+    {/* Here we use match.path, so we only have to worry about routing ahead of this point */}
+    {/* We only need to worry about /match.path/xyz, as it'll will give us whats behind  */}
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 export default ShopPage;
