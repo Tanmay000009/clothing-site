25357a4bfb7aaa398a4e8eef721fe88f8a637ccd
Tanmay000009 <tanmayvyas09@gmail.com>
2022-01-31 00:28:55 +0530

    ✨ get shop data from firebase

--- src/pages/shop/shop.componenet.jsx
+++ src/pages/shop/shop.componenet.jsx
@@ -3,12 +3,35 @@ import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    {/* Here we use match.path, so we only have to worry about routing ahead of this point */}
-    {/* We only need to worry about /match.path/xyz, as it'll will give us whats behind  */}
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+class ShopPage extends React.Component {
+  unSubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+
+  render() {
+    const match = this.props;
+    return (
+      <div className="shop-page">
+        {/* Here we use match.path, so we only have to worry about routing ahead of this point */}
+        {/* We only need to worry about /match.path/xyz, as it'll will give us whats behind  */}
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 export default ShopPage;
