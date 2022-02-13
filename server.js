a05410acb1f1cd0c8be3a584c7ce4b4ac5462c19
Tanmay000009 <tanmayvyas09@gmail.com>
2022-02-14 00:41:19 +0530

    ✨ add oayment route

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ import path from "path";
 import { error } from "console";
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+  import "dotenv/config";
 }
+
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +25,22 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
 app.listen(port, (err) => {
   if (err) throw error;
